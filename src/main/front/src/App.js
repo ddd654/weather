b0c282d31ff36b00b4df4f48ba9989e3ac2f1063
ddd654 <bound654@naver.com>
2024-10-08 17:27:01 +0900

    컴포넌트 넣기

--- src/main/front/src/App.js
+++ src/main/front/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 
+import Main from "./component/weather/Main";
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -15,6 +17,9 @@ function App() {
   return (
     <div>
       백 데이터 : {hello} 
+      <br></br>
+      1. 여기에 메인 레이아웃
+      <Main></Main>
       
     </div>
   );
