a8787b9d2318be8f8db5d6f0dee755125827841f
bound654 <bound654@naver.com>
2024-11-13 21:25:17 +0900

    css 연습

--- src/main/front/src/pages/po/Po.js
+++ src/main/front/src/pages/po/Po.js
@@ -1,14 +1,11 @@
 import Link from "next/link";
 import { list } from "postcss";
 import { useEffect, useState } from "react";
-import './Po.css';
-import '../../app/css/reset.css';
-
+import "./Po.css";
+import "../../app/css/reset.css";
 
 function Po() {
-
-  useEffect(() => {
-  }, []) //대괄호 넣으면 한번 실행
+  useEffect(() => { }, []); //대괄호 넣으면 한번 실행
 
   const [currentSlide, setCurrentslide] = useState(0); //현재 슬라이드
 
@@ -23,48 +20,81 @@ function Po() {
   }
 
   //지금 뭘 해야하지? 화면을 그리고 css로 위치를 잡는다
-  //화면을 여기에 만든다
+  // 화면을 여기에 만든다
   return (
     <div className="container">
-      <title>portfolio</title>
+      {/* <title>portfolio</title> */}
 
       {/* 헤더만 뽑아서 include 생각하기 메뉴 디자인은 부분은 비슷하게 가자 */}
-      <header>
+      {/* #붙여서 영역이동 가능 */}
+      {/* <header>
         <nav>
           <ul>
-            {/* #붙여서 영역이동 가능 */}
-
-            <li><Link href='/po/Intro' className="link" >Intro</Link></li>
-            <li><Link href='#home' className="link" >Home</Link></li>
-            <li><Link href='#about' className="link" >AboutMe</Link></li>
-            <li><Link href='#experience' className="link" >Project</Link></li>
-            <li><Link href='#contact' className="link" >Contact</Link></li>
-
-
+            <li>
+              <Link href="/po/Intro" className="link">
+                Intro
+              </Link>
+            </li>
+            <li>
+              <Link href="#home" className="link">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="#about" className="link">
+                AboutMe
+              </Link>
+            </li>
+            <li>
+              <Link href="#experience" className="link">
+                Project
+              </Link>
+            </li>
+            <li>
+              <Link href="#contact" className="link">
+                Contact
+              </Link>
+            </li>
           </ul>
         </nav>
-      </header>
+      </header> */}
 
       {/* slide 만들기 */}
       <main className="main">
-        <div className="slides"
+        <div
+          className="slides"
         //  style={{ transform: `translateX(-${currentSlide * 100}%)` }}
         >
-          <section id="home">home</section>
-          <section id="about">about</section>
-          <section id="experience">experience</section>
-          <section id="blackHole">바닥부분2222222</section>
-          <section id="contact">contact</section>
+          <section id="home">
+            home부분
 
+            {/* 임시용 */}
+            <section className="css">
+              <div>
+                <ul>
+                  <h2>dasd</h2>
+                  <p>apple, banana, kiwi</p>
+                  <li className="px_font">px</li>
+                  <li className="rem_font">rem</li>
+                </ul>
+              </div>
+              <Link href="https://www.naver.com">네이버</Link>
+              <a href="https://www.google.com">구글</a>
+            </section>
+
+          </section>
+
+          <section id="about">about부분</section>
+          <section id="experience">experience부분</section>
+          <section id="blackHole">바닥부분2</section>
+          <section id="contact">contact부분</section>
         </div>
       </main>
 
       {/* Footer 영역 */}
-      <footer>바닥부분</footer>
-
+      <footer>footer부분</footer>
     </div>
-  )
+  );
 }
 
 export default Po;
-
