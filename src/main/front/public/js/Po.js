3a544316f9022d4f38dc18a58f573ade65d1de1c
bound654 <bound654@naver.com>
2025-01-08 15:34:21 +0900

    js1

--- src/main/front/public/js/Po.js
+++ src/main/front/public/js/Po.js
@@ -1,1 +1,48 @@
-document.write('사과');
+// numbering = function () {
+//     i = 0;
+//     while (i < 10) {
+//         document.write(i);
+//         i += 1;
+//     }
+// }
+// function numbering() {
+//     i = 0;
+//     while (i < 10) {
+//         document.write(i);
+//         i++;
+//     }
+// }
+// numbering();
+
+// (function () {
+//     i = 0;
+//     while (i < 10) {
+//         document.write(i);
+//         i++;
+//     }
+// })();
+
+///////////////////////////
+
+// let tempFruit = 'subak';
+// let fruit = ['apple', 'banana', 'kiwi'];
+
+// document.write(fruit);
+
+// function get_fruit(){
+//     return ['apple', 'kiwi', 'peach'];
+// }
+
+// fruit = get_fruit();
+// document.write(' ' + fruit);
+
+// let member =['chris', 'jhon', 'ian']
+
+var member = ['apple', 'banana', 'kiwi']
+
+
+
+
+
+
+
---
+++ src/main/front/public/js_study/js_Hello.js
@@ -0,0 +1,10 @@
+(function () {
+    "use strict";
+    function greetMe(coco) {
+      alert(`Hello ${coco}`);
+    }
+  
+    greetMe("World");
+  })
+
+  
